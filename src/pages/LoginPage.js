422e057035968f0f6ba952d7a9d115d85d638534
Raktim Shrestha <raktim@rumsan.com>
2023-06-18 03:44:15 +0545

    login complete

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,7 +2,7 @@ import { Navigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 // @mui
 import { styled } from '@mui/material/styles';
-import { Alert, Link, Container, Typography, Divider, Stack, Button } from '@mui/material';
+import { Alert, Container, Typography, Stack, Button } from '@mui/material';
 // hooks
 import useResponsive from '../hooks/useResponsive';
 import { useMetaMask } from '../hooks/useMetaMask';
@@ -10,7 +10,6 @@ import { useMetaMask } from '../hooks/useMetaMask';
 import Logo from '../components/logo';
 import Iconify from '../components/iconify';
 // sections
-import { LoginForm } from '../sections/auth/login';
 
 // ----------------------------------------------------------------------
 
@@ -83,27 +82,10 @@ export default function LoginPage() {
               )}
             </Typography>
             <Typography variant="h4" gutterBottom>
-              Sign in to Minimal
-            </Typography>
-
-            <Typography variant="body2" sx={{ mb: 5 }}>
-              Don’t have an account?
-              <Link variant="subtitle2">Get started</Link>
+              Sign in to Chino
             </Typography>
 
             <Stack direction="row" spacing={2}>
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:google-fill" color="#DF3E30" width={22} height={22} />
-              </Button>
-
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:facebook-fill" color="#1877F2" width={22} height={22} />
-              </Button>
-
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={22} height={22} />
-              </Button>
-
               {window.ethereum?.isMetaMask && wallet.accounts.length < 1 && (
                 <Button
                   fullWidth
@@ -117,14 +99,6 @@ export default function LoginPage() {
                 </Button>
               )}
             </Stack>
-
-            <Divider sx={{ my: 3 }}>
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                OR
-              </Typography>
-            </Divider>
-
-            <LoginForm />
           </StyledContent>
         </Container>
       </StyledRoot>
